d38f23f34c6ce8772af139513dd33f8080ea154a
Rudy <rudybalzano16@gmail.com>
2021-12-06 11:34:43 +0100

    Swagger doc setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config =new DocumentBuilder()
+  .setTitle('PolyCovoit API Documentation')
+  .setDescription('A swagger documentation to explain all the routes')
+  .build();
+
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
