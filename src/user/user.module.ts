ea5448fc8891dd1aec4a3192c86d91ee1bf96f92
Rudy <rudybalzano16@gmail.com>
2021-12-06 16:26:17 +0100

    CRUD user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { databaseAccessModule } from 'src/utils';
+import { PasswordService } from "./password.service";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 
 @Module({
     imports: [databaseAccessModule()],
-    providers: [UserService],
+    providers: [UserService,PasswordService],
     controllers: [UserController]
 })
 
