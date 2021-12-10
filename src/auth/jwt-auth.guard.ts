366ad14d6dfb7dd12b3f0cde0d9bbe92dfb2a60b
Axel <axelcanton@hotmail.com>
2021-12-10 15:29:13 +0100

    tech: add custom error message on invalid token

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,7 +1,9 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from 'src/utils/roles/public.decorator';
+import { jwtConstants } from './constant';
+import { TokenExpiredError, JsonWebTokenError } from 'jsonwebtoken';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -18,5 +20,20 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
           return true;
         }
         return super.canActivate(context);
+      }
+
+      handleRequest(err, user, info) {
+        if(info instanceof TokenExpiredError) {
+          throw new UnauthorizedException(jwtConstants.expiredTokenErrorMessage);
+        }
+        if(info instanceof JsonWebTokenError) {
+          throw new UnauthorizedException(jwtConstants.malformedTokenErrorMessage);
+        }
+
+        if (err || !user) {
+          throw err || new UnauthorizedException();
+        }
+
+        return user;
       }
 }
