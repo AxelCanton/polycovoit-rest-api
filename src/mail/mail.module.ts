ed83b8eee32fe42990fe09aeb6f85470c69be245
Rudy <rudybalzano16@gmail.com>
2022-01-18 15:16:22 +0100

    mail connection

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -8,7 +8,15 @@ import { MailService } from './mail.service';
     imports: [
         MailerModule.forRootAsync({
           useFactory: () => ({
-            transport: process.env.MAIL_HOST,
+            transport: {
+              host: process.env.MAIL_HOST,
+              port: 587,
+              secure: false,
+              auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD,
+              },
+            },
             defaults: {
             from: '"PolyCovoit" <polycovoit@polycovoit.fr>',
           },
